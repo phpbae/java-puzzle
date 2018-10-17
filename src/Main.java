78022792857cb04275974dc31924c4f142d68bb3
kyle <kyle@freshcode.me>
2018-10-17 10:47:07 +0900

    - 자바퍼즐러 2 예제코드 작성.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,42 @@
 import puzzle1.홀수확인;
+import puzzle2.변화를위한시간;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        /**
-         * 홀수확인 : 2로 나눈 나머지가 1이면, 홀수
-         * 나머지 연산(%) -> 나머지 연산의 결과는 왼쪽 피연산자의 부호와 같다.(-3%2 = -1, 3%2=1)
-         */
-        System.out.println("1번째 퍼즐 - 홀수 확인");
+		/**
+		 * 홀수확인 : 2로 나눈 나머지가 1이면, 홀수 나머지 연산(%) -> 나머지 연산의 결과는 왼쪽 피연산자의 부호와 같다.(-3%2 =
+		 * -1, 3%2=1)
+		 */
+		System.out.println("1번째 퍼즐 - 홀수 확인");
 
-        System.out.println(홀수확인.isOdd(10)); //false
-        System.out.println(홀수확인.isOdd(9)); //true
-        // 음수인 경우? -> 음수가 들어오면 무조건 false를 리턴한다.
-        System.out.println(홀수확인.isOdd(-9)); //false
+		System.out.println(홀수확인.isOdd(10)); // false
+		System.out.println(홀수확인.isOdd(9)); // true
+		// 음수인 경우? -> 음수가 들어오면 무조건 false를 리턴한다.
+		System.out.println(홀수확인.isOdd(-9)); // false
 
-        // 양수/음수를 구분해서 해도 가능하나 -> 짝수면 2로 나눈 나머지가 0이기 때문에 이를 이용하여 처리한다. 홀수면 0이 아닐테니.
-        System.out.println(홀수확인.isOdd2(-9)); //true
-        System.out.println(홀수확인.isOdd2(-8)); //false
+		// 양수/음수를 구분해서 해도 가능하나 -> 짝수면 2로 나눈 나머지가 0이기 때문에 이를 이용하여 처리한다. 홀수면 0이 아닐테니.
+		System.out.println(홀수확인.isOdd2(-9)); // true
+		System.out.println(홀수확인.isOdd2(-8)); // false
 
-        // 만약 성능이 중요하다면? 비트연산자를 이용
-        System.out.println(홀수확인.isOdd3(-7)); //true
-        System.out.println(홀수확인.isOdd3(-5)); //true
-        System.out.println(홀수확인.isOdd3(7)); //true
-        System.out.println(홀수확인.isOdd3(10)); //false
+		// 만약 성능이 중요하다면? 비트연산자를 이용
+		System.out.println(홀수확인.isOdd3(-7)); // true
+		System.out.println(홀수확인.isOdd3(-5)); // true
+		System.out.println(홀수확인.isOdd3(7)); // true
+		System.out.println(홀수확인.isOdd3(10)); // false
 
-    }
+		/**
+		 * 변화를 위한 시간 : 2.00 - 1.10 = 0.90 ? double은 숫자를 최소자릿수로 출력. 
+		 * 자바에서 모든 소수가 float 또는 double로 표현될 수 있는 것은 아님.
+		 * 자바에서 float / double 자료형으로 정확한 연산을 수핼할 수 없으므로, 금융계산 및 정밀한 계산이 필요한 곳에서 사용하면 안된다.
+		 * 자바에서는 이진부동소수점 연산을 사용 : 계산은 빠르지만, 미세한 오차가 있음. 오차가 없는 십진부동소수점연산도 존재하지만 자바에서는 미지원
+		 * 정확한 연산값을 원하면, int/long/BigDecimal을 이용하자!
+		 */
+		변화를위한시간.printMoney(); //0.8999999999999999	
+		변화를위한시간.printMoney2(); //0.90 printf() 이용
+		변화를위한시간.printMoney3(); //int / long 이용해서 처리
+		변화를위한시간.printMoney4(); //BigDecimal 클래스 이용
+
+	}
 }
---
+++ src/puzzle2/변화를위한시간.java
@@ -0,0 +1,57 @@
+package puzzle2;
+
+import java.math.BigDecimal;
+
+public class 변화를위한시간 {
+
+	
+	/**
+	 * 1.10 달러 점화 플러그 구입. 지불을 2달러를 제시.
+	 * 2.00 - 1.10 -> 받을 거스름 돈은?
+	 */
+	
+	public static void printMoney() {
+		double a = 2.00;
+		double b = 1.10;
+		System.out.println(a - b); // 0.90 ?
+	}
+	
+	/**
+	 * printf() 를 활용하여 0.90이 나오게 할 수 있다.
+	 * 하지만, 좋은 방법은 아니다. 여전히 부동소수점 연산 + 반올림 되어서 출력이 될뿐 
+	 */
+	public static void printMoney2() {
+		double a = 2.00;
+		double b = 1.10;
+		System.out.printf("%.2f \n", a-b);
+	}
+	
+	/**
+	 * int / long 정수 자료형을 이용하면 위와 같은 문제를 해결할 수 있다.
+	 * 다만, int / long 같은 경우는 숫자 범위를 조심(오버플로우 발생)
+	 */
+	public static void printMoney3() {
+		int a = 200;
+		int b = 110;
+		System.out.println(a-b + " cents");
+	}
+	
+	/**
+	 * BigDecimal 클래스 이용
+	 */
+	public static void printMoney4() {
+		double a = 2.00;
+		double b = 1.10;
+		//주의 : 생성자에 매개변수로 입력한 값을 그대로 표현
+		BigDecimal decimalDouble = new BigDecimal(b); // 1.10 = 1.100000000000000088817841970012523233890533447265625
+		BigDecimal decimalString = new BigDecimal("2.00"); // String 생성자를 이용하자.
+		System.out.println("decimalDouble : " + decimalDouble);
+		System.out.println("decimalString : " + decimalString);
+		System.out.println("2.00 - 1.10 = " + new BigDecimal("2.00").subtract(new BigDecimal("1.10")));
+		
+		
+		
+		
+	}
+	
+}
